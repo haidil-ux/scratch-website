eff7ee03ae5aba6d671dc25713d597d19bf48d04
haidil-ux <72535536+haidil-ux@users.noreply.github.com>
2024-01-03 10:40:51 +0800

    Add files via upload

---
+++ prototype webpage/admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+</style>
+</head>
+<body>
+<div class="">
+      <div class=""><a href="" class="material-icons">supervisor_account ADMIN<a></div>
+      <div class="">LOGOUT</div>
+</div>
+
+</body>
+</html>
